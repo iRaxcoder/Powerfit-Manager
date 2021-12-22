e5cc20600e2f69bcf9ebba2dd7710ed72d7a1d03
Randall <randms07@hotmail.com>
2021-12-22 16:24:14 -0600

    layout added

--- Client/powerfit_manager_client/src/App.js
+++ Client/powerfit_manager_client/src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <Router>
-            <Sidebar/>
             <Routes>
                 {routes.map(route=>(
                     <Route
