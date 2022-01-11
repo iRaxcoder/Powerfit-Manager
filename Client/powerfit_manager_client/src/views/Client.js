c302606a8a33ca69024b1543eec62b8bc88fd7a6
Randall <randms07@hotmail.com>
2022-01-11 10:20:33 -0600

    sql script modified and insert and edit client finished

--- Client/powerfit_manager_client/src/views/Client.js
+++ Client/powerfit_manager_client/src/views/Client.js
@@ -12,94 +12,82 @@ export default function Client(){
   const [isOpenInsert, setIsOpenInsert] = useState(false);
   const [isOpenEdit, setIsOpenEdit] = useState(false);
   const [isOpenDelete, setIsOpenDelete] = useState(false);
-  const [exerciseEdited, setExerciseEdited] = useState({id:0, exercise:"default", muscleGroup:"dafault"});
-  const [exercisesList, setExercisesList] = useState(null);
-  const [muscleGroupList, setMuscleGroupList] = useState(null);
-  const exercisesListRef= useRef();
+  const [clientEdited, setClientEdited] = useState({id:0, name:"def", lastName:"def", age:0, number: "def", email:"def", illness: "def"});
+  const [clientList, setClientList] = useState(null);
+  const clientsListRef= useRef();
   const [modalMsg, setModalMsg]= useState({isMsgOpen: false, msg: ""});
-  exercisesListRef.current=exercisesList;
+  clientsListRef.current=clientList;
     
   const columns = React.useMemo(
       () => [
-        {
-          Header: '#',
-          accessor: 'ID_EJERCICIO',
-          },
-          {
-            Header: 'Ejercicio',
-            accessor: 'NOMBRE_EJERCICIO',
-          },
-          {
-            Header: 'Grupo muscular',
-            accessor: 'NOMBRE_GRUPO_MUSCULAR',
-          },
+          { Header: '#', accessor: 'ID_CLIENTE',},
+          { Header: 'Nombre', accessor: 'NOMBRE_CLIENTE'},
+          { Header: 'Apellidos',accessor: 'APELLIDOS'},
+          { Header: 'Edad',accessor: 'EDAD'},
+          { Header: 'Teléfono',accessor: 'TELEFONO'},
+          { Header: 'Correo', accessor: 'EMAIL'},
+          { Header: 'Enfermedades', accessor: 'ENFERMEDAD'},
         ],
       []
   )
 
-  const fetchExercises = () => {
-      commonDB.getAll({header:"ejercicio"}).then(response=>{
-      setExercisesList(response)
-    })
-  }
-
-  const fetchMuscleGroups = () => {
-    commonDB.getAll({header:"grupo_muscular"}).then(response=>{
-      setMuscleGroupList(response)
+  const fetchClients = () => {
+      commonDB.getAll({header:"cliente"}).then(response=>{
+      setClientList(response)
     })
   }
   
   useEffect(()=>{
-    fetchExercises();
-    fetchMuscleGroups();
+    fetchClients();
   },[]);
 
-  if(!exercisesList) return "No se encuentran ejercicios";
+  if(!clientList) return "No se encuentran ejercicios";
     
     const handleInsert = (e) => {
-      commonDB.insert({header:"ejercicio",size:"2", object: e}).then(response=>{   
+      commonDB.insert({header:"cliente",size:"6", object: e}).then(response=>{   
           setModalMsg(prevState =>({
             ...prevState,
             msg: response,
             isMsgOpen: true
           }));
-        fetchExercises();
+        fetchClients();
       })
       setIsOpenInsert(false);
     }
 
     const HandleOpenEdit = (e) => {
-      const exercise = JSON.parse(e.target.dataset.row);
-      setExerciseEdited({id:exercise.ID_EJERCICIO,exercise:exercise.NOMBRE_EJERCICIO, muscleGroup: exercise.NOMBRE_GRUPO_MUSCULAR})
+      const client = JSON.parse(e.target.dataset.row);
+      setClientEdited(
+      {id:client.ID_CLIENTE,name:client.NOMBRE_CLIENTE, lastName: client.APELLIDOS, age: client.EDAD, number: client.TELEFONO, email: client.EMAIL, illness: client.ENFERMEDAD})
       setIsOpenEdit(true);
     }
 
     const HandleEdit = () => {
-        commonDB.update({header:"ejercicio",size:"3", object: exerciseEdited}).then(response=>{   
+        commonDB.update({header:"cliente",size:"7", object: clientEdited}).then(response=>{   
           setModalMsg(prevState =>({
             ...prevState,
             msg: response,
             isMsgOpen: true
           }));
-          fetchExercises();
+          fetchClients();
         })
       setIsOpenEdit(false);
     }
 
     const HandleOpenDelete = (e) => {
       const exercise = JSON.parse(e.target.dataset.row);
-      setExerciseEdited({id:exercise.ID_EJERCICIO,exercise:exercise.NOMBRE_EJERCICIO})
+      setClientEdited({id:exercise.ID_EJERCICIO,exercise:exercise.NOMBRE_EJERCICIO})
       setIsOpenDelete(true);
     }
 
     const HandleDelete = () => {
-      commonDB.delete({header:"ejercicio", object: {id:exerciseEdited.id}}).then(response=>{   
+      commonDB.delete({header:"cliente", object: {id:clientEdited.id}}).then(response=>{   
         setModalMsg(prevState =>({
           ...prevState,
           msg: response,
           isMsgOpen: true
         }));
-        fetchExercises();
+        fetchClients();
       })
     setIsOpenDelete(false);
     }
@@ -107,61 +95,71 @@ export default function Client(){
     const handleSearch = (e) => {
       console.log(e.target.value);
       if(e.target.value===undefined || e.target.value ===""){
-        fetchExercises();
+        fetchClients();
       }else{
         commonDB.getSearch({header: "ejercicio",find:e.target.value}).then(response=>{
-          setExercisesList(response);
+          setClientList(response);
         })
       }
     }
 
     return (
         <div>
-            <h1 className="text-left">Control de ejercicios</h1>
+            <h1 className="text-left">Control de clientes</h1>
             <hr/>
             <div className="container">
                 <div className="container-insert-search__">
-                  <AddButton text="Insertar" onClick={()=>setIsOpenInsert(true)} />
+                  <div>
+                    <AddButton text="Insertar" onClick={()=>setIsOpenInsert(true)} />
+                  </div>
                   <SingleCustomInput onChange={handleSearch} errorMsg="hahaha nombre del ejercicio" placeholder="Buscar" name="input-search" className="form-control"/>
                 </div>    
                 <Table
                   columns={columns}
-                  data={exercisesList}
-                  aux={exercisesListRef.current}
+                  data={clientList}
+                  aux={clientsListRef.current}
                   funEdit={HandleOpenEdit}
                   funDelete={HandleOpenDelete}
                 />
             </div>
             <CustomModal
-              props={{title: 'Insertar ejercicio', isOpen: isOpenInsert}}
+              props={{title: 'Insertar cliente', isOpen: isOpenInsert}}
               methods={{toggleOpenModal: ()=>setIsOpenInsert(!isOpenInsert)}}
                 >
               <CustomForm onSubmit={handleInsert}>
-                <CustomInput errorMsg="Inserte nombre del ejercicio" className='form-control mt-2' name='exercise_insert' placeholder='Nombre ejercicio'></CustomInput>
-                <CustomSelect focus="NOMBRE_GRUPO_MUSCULAR" errorMsg="Seleccione un grupo muscular"  className='form-control mt-2' name='muscule_group_insert' placeholder='Nombre grupo muscular' options={muscleGroupList}></CustomSelect>
+                <CustomInput errorMsg="Nombre requerido" className='form-control mt-2' name='client_name_insert' placeholder='Nombre'></CustomInput>
+                <CustomInput errorMsg="Apellidos requeridos" className='form-control mt-2' name='last_name_insert' placeholder='Apellidos'></CustomInput>
+                <CustomInput errorMsg="Edad requerida" className='form-control mt-2' name='age_name_insert' placeholder='Edad'></CustomInput>
+                <CustomInput errorMsg="Teléfono requerido" className='form-control mt-2' name='number_insert' placeholder='Teléfono'></CustomInput>
+                <CustomInput errorMsg="Correo requerido" className='form-control mt-2' name='email_insert' placeholder='Email'></CustomInput>
+                <CustomInput errorMsg="Este campo es requerido" className='form-control mt-2' name='illness_insert' placeholder='Enfermedad'></CustomInput>
                 <AddButton text="Insertar"/>
                 <CancelButton fun={()=>setIsOpenInsert(false)}/>
               </CustomForm>
             </CustomModal>
             <CustomModal
-              props={{title: 'Modificar ejercicio', isOpen: isOpenEdit}}
+              props={{title: 'Modificar cliente', isOpen: isOpenEdit}}
               methods={{toggleOpenModal: ()=>setIsOpenEdit(!isOpenEdit)}}
                 >
               <CustomForm onSubmit={HandleEdit}>
-                <CustomInput type="hidden" value={exerciseEdited.id} className='form-control mt-2' name='exerciseId'/>
-                <CustomInput name='exerciseEdit' onChange={(e)=>setExerciseEdited(prevState =>({...prevState,exercise:e.target.value}))} value={exerciseEdited.exercise} errorMsg="Escriba el nombre del ejercicio" className='form-control mt-2' placeholder='Nombre ejercicio'/>
-                <CustomSelect value={exerciseEdited.muscleGroup} onChange={(e)=>setExerciseEdited(prevState =>({...prevState,muscleGroup:e.target.value}))} focus="NOMBRE_GRUPO_MUSCULAR" errorMsg="Seleccione un grupo muscular"  className='form-control mt-2' name='muscleGroupSelect' placeholder='Nombre grupo muscular' options={muscleGroupList}/>
+                <CustomInput type="hidden" value={clientEdited.id} className='form-control mt-2' name='client_id_edit'/>
+                <CustomInput errorMsg="Nombre requerido" onChange={(e)=>setClientEdited(prevState =>({...prevState,name:e.target.value}))} value={clientEdited.name} className='form-control mt-2' name='client_name_edit' placeholder='Nombre'></CustomInput>
+                <CustomInput errorMsg="Apellidos requeridos" onChange={(e)=>setClientEdited(prevState =>({...prevState,lastName:e.target.value}))} value={clientEdited.lastName} className='form-control mt-2' name='last_name_editt' placeholder='Apellidos'></CustomInput>
+                <CustomInput errorMsg="Edad requerida" onChange={(e)=>setClientEdited(prevState =>({...prevState,age:e.target.value}))} value={clientEdited.age} className='form-control mt-2' name='age_name_edit' placeholder='Edad'></CustomInput>
+                <CustomInput errorMsg="Teléfono requerido" onChange={(e)=>setClientEdited(prevState =>({...prevState,number:e.target.value}))}value={clientEdited.number} className='form-control mt-2' name='number_edit' placeholder='Teléfono'></CustomInput>
+                <CustomInput errorMsg="Correo requerido"onChange={(e)=>setClientEdited(prevState =>({...prevState,email:e.target.value}))} value={clientEdited.email} className='form-control mt-2' name='email_edit' placeholder='Email'></CustomInput>
+                <CustomInput errorMsg="Este campo es requerido"onChange={(e)=>setClientEdited(prevState =>({...prevState,illness:e.target.value}))} value={clientEdited.illness} className='form-control mt-2' name='illness_edit' placeholder='Enfermedad'></CustomInput>
                 <AddButton type="submit" text="Guardar cambios"/>
                 <CancelButton fun={()=>setIsOpenEdit(false)}/>
               </CustomForm>
             </CustomModal>
             <CustomModal
-              props={{title: "¿Está seguro que desea eliminar '"+ exerciseEdited.exercise+"'?", isOpen: isOpenDelete}}
+              props={{title: "¿Está seguro que desea eliminar el cliente '"+ clientEdited.name+"'?", isOpen: isOpenDelete}}
               methods={{toggleOpenModal: ()=>setIsOpenDelete(!isOpenDelete)}}
                 >
               <CustomForm onSubmit={HandleDelete}>
-                <CustomInput type="hidden" value={exerciseEdited.id} className='form-control mt-2' name='exerciseId'/>
-                <AddButton text="Sí, estoy seguro"/>
+                <CustomInput type="hidden" value={clientEdited.id} className='form-control mt-2' name='exerciseId'/>
+                <AddButton text="Sí, estoy seguro."/>
                 <CancelButton fun={()=>setIsOpenDelete(false)}/>
               </CustomForm>
             </CustomModal>
