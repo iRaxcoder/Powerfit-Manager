80f2276b1428918427d9bb740ffc621c90481161
Randall <randms07@hotmail.com>
2022-02-07 14:44:09 -0600

    insert routine added

---
+++ Client/powerfit_manager_client/src/service/Routine.js
@@ -0,0 +1,22 @@
+import {
+    BASE_URL
+} from '../base.js'
+import axios from 'axios';
+
+const RoutineDB = {
+    insert: async (data) => {
+        //loading.style.display="block";
+        const response = await axios.post(BASE_URL + '/routine/insert', { data })
+            .then((response) => {
+                return response.data;
+            }).catch(error => {
+                console.log(error);
+            })
+            .finally(() => {
+                //loading.style.display="none";
+            });
+        return response;
+    },
+}
+
+export default RoutineDB;
--- Server/controllers/routine.js
+++ Server/controllers/routine.js
@@ -1,3 +1,63 @@
+
+var routineId=0;
+
+const getSpParamSize = {
+    1: '(?)',
+    2: '(?,?)',
+    3: '(?,?,?)',
+    4: '(?,?,?,?)',
+    5: '(?,?,?,?,?)',
+    6: '(?,?,?,?,?,?)',
+    7: '(?,?,?,?,?,?,?)',
+    8: '(?,?,?,?,?,?,?,?)',
+    9: '(?,?,?,?,?,?,?,?,?)',
+    10: '(?,?,?,?,?,?,?,?,?,?)',
+    11: '(?,?,?,?,?,?,?,?,?,?,?)',
+}
+
 module.exports.set = function(app,connection){
-    
+    app.post("/routine/insert", (req, res) => {
+        data = req.body.data;
+        connection.beginTransaction(async (err) => {
+            if (err) {
+                throw err;
+            }
+            query = 'CALL sp_insert_rutina' + getSpParamSize[data["size"]];
+            connection.query(query, Object.values(data["generalInfo"]), (err, rows, fields) => {
+                if (!err) {
+                    routineId = rows[0][0].ID_RUTINA;
+                    console.log("ID_RUTINA: "+routineId)
+                } else {
+                    connection.rollback(() => {
+                        res.send("No se ha registrar la rutina");
+                        console.log(err);
+                    });
+                }
+                query = 'INSERT INTO tb_rutina_ejercicio (ID_RUTINA,ID_EJERCICIO,DIA,INDICACIONES) VALUES ?';
+                var generalExerciseList = data["exerciseList"];
+                var exerciseListInsert = [generalExerciseList.length];
+                for (var i = 0; i < generalExerciseList.length; i++) {
+                    exerciseListInsert[i] = [routineId, generalExerciseList[i].idExercise, generalExerciseList[i].day, generalExerciseList[i].details];
+                }
+                connection.query(query, [exerciseListInsert], (err, rows, fields) => {
+                    if (err) {
+                        connection.rollback(() => {
+                            res.send("No se ha podido registrar la rutina.");
+                            console.log(err);
+                        })
+                    }
+                    connection.commit((err) => {
+                        if (!err) {
+                            res.send("rutina realizada con éxito.");
+                        } else {
+                            connection.rollback(() => {
+                                res.send("No se ha podido realizar la rutina.");
+                                console.log(err);
+                            })
+                        }
+                    })
+                });
+            });
+        })
+    })
 }
