9f58c438279fc022a804faf4a03848571babf34b
Randall <randms07@hotmail.com>
2022-02-10 11:33:48 -0600

    login security added

--- Client/powerfit_manager_client/src/App.js
+++ Client/powerfit_manager_client/src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import routes from './routes/routes';
+import AuthContext from './components/hooks/Authentication/AuthContext';
+import { useContext } from 'react';
 
 function App() {
+  const {userAuth}=useContext(AuthContext);
   return (
     <div className="App">
       <Router>
             <Routes>
+              <Route path='/'
+              element={routes[1].element}/>
                 {routes.map(route=>(
                     <Route
                         path={route.path}
-                        element={route.element}
+                        element={userAuth?route.element:(
+                          <>
+                          {routes[1].element}
+                          </>
+                        )}
                     />
                 ))}
             </Routes>
